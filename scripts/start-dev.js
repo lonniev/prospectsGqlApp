93a9baa386fb89f73bcaf6545d751c1d483a182d
Lonnie VanZandt <lonnie.vanzandt@intercax.com>
2021-11-24 17:35:27 -0500

    remove unused grand-stack-starter stuff

--- scripts/start-dev.js
+++ scripts/start-dev.js
@@ -1,12 +1,6 @@
 const concurrently = require('concurrently')
 
-const {
-  API_DIR,
-  TEMPLATE_DIR,
-  runner,
-  concurrentOpts,
-  templateName,
-} = require('./common')
+const { API_DIR, runner, concurrentOpts } = require('./common')
 
 const jobs = [
   {
@@ -15,20 +9,6 @@ const jobs = [
     prefixColor: 'green',
   },
 ]
-
-if (templateName === 'Flutter') {
-  jobs.push({
-    name: templateName,
-    command: `cd ${TEMPLATE_DIR} && flutter run`,
-    prefixColor: 'blue',
-  })
-} else {
-  jobs.push({
-    name: templateName,
-    command: `cd ${TEMPLATE_DIR} && ${runner} run start`,
-    prefixColor: 'blue',
-  })
-}
 
 concurrently(jobs, concurrentOpts).catch((e) => {
   console.error(e.message)
